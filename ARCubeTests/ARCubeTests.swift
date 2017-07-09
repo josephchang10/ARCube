42624f6630847aedb301016eb3a6b0b71104c1ed
张嘉夫 <joseph@fanruan.com>
2017-07-09 08:40:52 +0800

    Initial Commit

---
+++ ARCubeTests/ARCubeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARCubeTests.swift
+//  ARCubeTests
+//
+//  Created by 张嘉夫 on 2017/7/9.
+//  Copyright © 2017年 张嘉夫. All rights reserved.
+//
+
+import XCTest
+@testable import ARCube
+
+class ARCubeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
